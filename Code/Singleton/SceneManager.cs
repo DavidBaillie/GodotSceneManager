546f0312ffeeaa13737f31ba9a08fe2117a39003
DavidBaillie <DavidlBaillie@gmail.com>
2024-05-20 14:28:11 -0400

    Adding logic for scene transition

--- Code/Singleton/SceneManager.cs
+++ Code/Singleton/SceneManager.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System.Collections.Generic;
 
 [GlobalClass]
 public partial class SceneManager : Node
@@ -11,7 +12,7 @@ public partial class SceneManager : Node
     /// Loads the provided game mode and scenes for the mode
     /// </summary>
     /// <param name="gameMode">Gamemode to load</param>
-    public void LoadGameMode(SceneCollectionTag collection) 
+    public void LoadGameMode(SceneCollectionTag collection, bool freeUntrackedScenes = true) 
     {
         if (collection == null)
         {
@@ -25,15 +26,72 @@ public partial class SceneManager : Node
             return;
         }
 
-        CallDeferred(MethodName.LoadSceneCollection_Deferred, collection);
+        CallDeferred(MethodName.LoadSceneCollection_Deferred, collection, freeUntrackedScenes);
     }
-    private void LoadSceneCollection_Deferred(SceneCollectionTag collection)
+    private void LoadSceneCollection_Deferred(SceneCollectionTag collection, bool freeUntrackedScenes = true)
     {
+        // TODO - add loading screen
+
         if (CurrentCollection?.GameMode != null )
         {
             CurrentCollection.GameMode.Cleanup(CurrentBaseNode);
         }
 
-        
+        var nodeActions = GenerateSceneActions(collection);
+
+
+
+        // TODO - unload actions
+
+        // TODO - load actions
+
+        // TODO - start new gamemode
+
+        // TODO - remove loading screen
+    }
+
+    /// <summary>
+    /// Determines what load actions need to be taken to transition from one scene collection to another
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <returns></returns>
+    private SceneActionCollection GenerateSceneActions(SceneCollectionTag collection)
+    {
+        var existingActions = GenerateActionsForExistingScenes(collection);
+        var newActions = GenerateActionsForNewCollectionScenes(collection);
+
+        return new SceneActionCollection()
+        {
+            ScenesToLoad = newActions,
+            NodesToTransfer = existingActions.toMove,
+            NodesToUnload = existingActions.toUnload,
+            UntrackedNodes = existingActions.unknown
+        };
+    }
+
+    /// <summary>
+    /// Given a collection, determines what nodes need to be unloaded and what nodes are 'unknown'
+    /// </summary>
+    /// <param name="collection">Collection being loaded</param>
+    private (List<Node> toUnload, List<Node> toMove, List<Node> unknown) GenerateActionsForExistingScenes(SceneCollectionTag collection)
+    {
+        return default;
+    }
+
+    /// <summary>
+    /// Given a collection, determines whick of the nodes need to be loaded into the game
+    /// </summary>
+    /// <param name="collection">Collection being loaded</param>
+    private List<PackedScene> GenerateActionsForNewCollectionScenes(SceneCollectionTag collection)
+    {
+        return default;
+    }
+
+    private struct SceneActionCollection
+    {
+        public List<PackedScene> ScenesToLoad { get; set; }
+        public List<Node> NodesToTransfer { get; set; }
+        public List<Node> NodesToUnload { get; set; }
+        public List<Node> UntrackedNodes { get; set; }
     }
 }
--- Code/Tags/SceneCollectionTag.cs
+++ Code/Tags/SceneCollectionTag.cs
@@ -7,6 +7,12 @@ public partial class SceneCollectionTag : Tag
     [Export]
     public string EditorDisplayName { get; set; }
 
+    /// <summary>
+    /// When a required node in this collection already exists in the previous collection, should the transition reload that node or keep it?
+    /// </summary>
+    [Export]
+    public bool ReloadAlreadyExistingNodes { get; set; } = false;
+
     [Export]
     public GameModeTag GameMode { get; set; }
 
