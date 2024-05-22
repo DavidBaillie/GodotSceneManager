1fdbf000be01d8388875e41903f3f838bb5e9452
DavidBaillie <DavidlBaillie@gmail.com>
2024-05-21 20:28:41 -0400

    Fixed system

--- Code/Common/MAIN.cs
+++ Code/Common/MAIN.cs
@@ -7,24 +7,24 @@ public partial class MAIN : Node3D
     [Export]
     public PackedScene TScene { get; set; }
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-	{
-		var scene = GD.Load<PackedScene>(TScene.ResourcePath).Instantiate();
-		GetTree().Root.CallDeferred(MethodName.AddChild, scene);
+ //   // Called when the node enters the scene tree for the first time.
+ //   public override void _Ready()
+	//{
+	//	var scene = GD.Load<PackedScene>(TScene.ResourcePath).Instantiate();
+	//	GetTree().Root.CallDeferred(MethodName.AddChild, scene);
 
-		CallDeferred(MethodName.CheckAfter);
-	}
+	//	CallDeferred(MethodName.CheckAfter);
+	//}
 
-	public void CheckAfter()
-	{
-		foreach (var node in GetTree().Root.GetChildren())
-		{
-			GD.Print($"IsSubclass: {node.GetType().IsSubclassOf(typeof(Node))}");
-			//GD.Print($"{node.Name} --> IsControl: {node is Control}, IsNode3D: {node is Node3D}, IsNode2D: {node is Node2D}");
-		}
+	//public void CheckAfter()
+	//{
+	//	foreach (var node in GetTree().Root.GetChildren())
+	//	{
+	//		GD.Print($"IsSubclass: {node.GetType().IsSubclassOf(typeof(Node))}");
+	//		//GD.Print($"{node.Name} --> IsControl: {node is Control}, IsNode3D: {node is Node3D}, IsNode2D: {node is Node2D}");
+	//	}
 
 
-        //GD.Print(string.Join(",", GetTree().Root.GetChildren().ToList().Select(x => x.Name)));
-    }
+ //       //GD.Print(string.Join(",", GetTree().Root.GetChildren().ToList().Select(x => x.Name)));
+    //}
 }
